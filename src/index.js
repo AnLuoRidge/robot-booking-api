5c589912e7b1d4298fbb191aefe3b581f5aa174b
anluoridge <anluoridge@gmail.com>
2019-11-15 12:09:00 +1100

    Add fake bookable days

--- src/index.js
+++ src/index.js
@@ -1,14 +1,63 @@
 import 'dotenv/config';
 import cors from 'cors';
 import express from 'express';
+import { mockBookableDaysDB } from './mock-data';
 
 const app = express();
 app.use(cors());
 
+const getBookableDays = (year, month) => {
+    console.log(`GET Year: ${year} Month: ${month}\n`);
+
+    if (month <= 0 || month > 12) {
+        // TODO: month in [1, 2, 3]
+        // TODO: params missing
+        return {
+        "success": false,
+        "error": "Invalid month"
+      };
+    }
+    // Search
+    // const db = express.json(mockBookableDaysDB);
+    if(mockBookableDaysDB.hasOwnProperty(year)) {
+        
+        if (mockBookableDaysDB[year].hasOwnProperty(month)) {
+            const bookableDays = mockBookableDaysDB[year][month];
+            return bookableDays;
+        } else {
+            return {
+                "success": false,
+                "error": "Invalid month"
+              };
+        }
+    } else {
+        if (process.env.NODE_ENV === 'debug') {
+            console.log(`Invalid year: ${year}`);
+        }
+        return {
+            "success": false,
+            "error": "Invalid month"
+          };
+    }
+
+
+    
+    // const mockReturn = {
+    //     "success": true,
+    //     "days": [
+    //       { "day": 1,  "hasTimeSlots": false },
+    //       { "day": 31, "hasTimeSlots": true }
+    //     ]
+    //   };
+    // return mockReturn;
+}
+
 // Routes which would be refactored
 app.get('/days', (req, res) => {
-  return res.send(`GET Year: ${req.query.year} Month: ${req.query.month}\n`);
-});// TODO: Validator: Month X 00
+    // TODO: Validator: Month X 00
+    const bookableDays = getBookableDays(req.query.year, req.query.month);
+    return res.send(bookableDays);
+});
 
 app.listen(process.env.PORT, () =>
   console.log(`App listening on port ${process.env.PORT}!`),
---
+++ src/mock-data.js
@@ -0,0 +1,23 @@
+export const mockBookableDaysDB = {
+    "2019": {
+        "1": [
+            { "day": 1,  "hasTimeSlots": false },
+            { "day": 31, "hasTimeSlots": true }
+        ],
+        "2": [
+            { "day": 2,  "hasTimeSlots": true },
+            { "day": 28, "hasTimeSlots": true }
+        ]
+    },
+    "2018": {
+        "1": [
+            { "day": 3,  "hasTimeSlots": false },
+            { "day": 4,  "hasTimeSlots": true },
+            { "day": 31, "hasTimeSlots": false }
+        ],
+        "2": [
+            { "day": 4,  "hasTimeSlots": false },
+            { "day": 28, "hasTimeSlots": true }
+        ]
+    }
+};
