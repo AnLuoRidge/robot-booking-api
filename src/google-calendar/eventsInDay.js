b8e005f008da14798f95c80e171fe93d5cd1da79
anluoridge <anluoridge@gmail.com>
2019-11-21 10:59:29 +1100

    Refactor eventsInDay

--- src/google-calendar/eventsInDay.js
+++ src/google-calendar/eventsInDay.js
@@ -1,16 +1,26 @@
-const allTimeSlotsAt = (year, month, day) => {
-  const allTimeSlots = [];
-  const startTime = new Date(`${year}-${month}-${day}T09:00:00Z`);
-  for (let i = 0; i < 12; i ++) {
-    const startTimeString = startTime.toISOString();
-    startTime.setMinutes(startTime.getMinutes() + 40);
-    const endTimeString = startTime.toISOString();
-    const timeSlot = {
-      'startTime': startTimeString,
-      'endTime': endTimeString,
-    };
-    allTimeSlots.push(timeSlot);
-    startTime.setMinutes(startTime.getMinutes() + 5);
+import Calendar from './calendar';
+import getEvents from './getEvents';
+
+/*
+const Calendar = require('./calendar');
+const eventList = require('./eventList');
+const logger = require('../config/winston');
+const errorMsg = require('../config/errorMessages.json');
+*/
+
+const eventsInDay = async (year, month, day) => {
+    let calendar = (new Calendar()).calendar;
+    const options = {
+        timeMin: `${year}-${month}-${day}T9:00:00.000Z`,
+        timeMax: `${year}-${month}-${day}T18:00:00.000Z`,
+        maxResults: global.gConfig.TIME_SLOTS_WHOLE_DAY,
+        singleEvents: true,
+        orderBy: 'startTime',
+       calendarId: global.gConfig.GOOGLE_CALENDAR_ID
+    }
+    const result = await getEvents(calendar, options);
+    return result;
   }
-  return allTimeSlots;
-};
+
+export default eventsInDay;
+// module.exports(eventsInDay);
--- src/routes/available-time-slots.js
+++ src/routes/available-time-slots.js
@@ -1,30 +1,77 @@
+// const logger = require('../config/winston');
 import logger from '../config/winston';
-import { Router } from 'express';
-// import { mockBookableDaysDB } from '../mock-data';
+const { Router } = require('express');
+import eventsInDay from '../google-calendar/eventsInDay';
+
 
 const router = Router();
-
 logger.debug('Loading available time slots route');
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
     logger.debug('Executing available time slot route');
     logger.debug(`Year: ${req.query.year} Month: ${req.query.month} Day: ${req.query.day}`);
-    
-    const mockTimeSlots = {
-        "success": true,
-        "timeSlots": [
-          {
-            "startTime": "2019-09-04T09:00:00.000Z",
-              "endTime": "2019-09-04T09:40:00.000Z"
-          },
-          {
-            "startTime": "2019-09-04T09:45:00.000Z",
-              "endTime": "2019-09-04T10:25:00.000Z"
-          },
-        ]
-      };
-    return res.send(mockTimeSlots);
+    const result = await getAvailableTimeSlots(req.query.year, req.query.month, req.query.day);
+    res.send(result);
 });
 
+const getAvailableTimeSlots = async (year, month, day) => {
+  const res = await eventsInDay(year, month, day);
+
+  if (res.success) {
+    const events = res.events;
+    if (events.length > global.gConfig.TIME_SLOTS_WHOLE_DAY - 1) {      
+      logger.info(errorMsg.dayNotAvailable)
+      return errorMsg.dayNotAvailable; 
+    } else {
+      logger.info('Events:')
+
+      // Extract start time and end time from the event list.
+      var appointments = events.map(event => {
+        const start = event.start.dateTime || event.start.date;
+        const end = event.end.dateTime || event.end.date;
+        
+        logger.info(`${start} - ${event.summary}`);
+        logger.info(new Date(start).getDate());
+
+        var timeSlot = {
+            "startTime": new Date(start).toISOString(),
+            "endTime": new Date(end).toISOString()
+          }
+        return JSON.stringify(timeSlot);
+      });
+
+      const allTimeSlots = allTimeSlotsAt(year, month, day);
+
+      // Get difference set by 'all time slots - appointments'
+      var availableTimeSlots = allTimeSlots.filter(timeSlot => 
+        !appointments.includes(JSON.stringify(timeSlot))
+      )
+
+        availableTimeSlots = {
+          "success": true,
+          "timeSlots": availableTimeSlots
+        }
+        logger.info(JSON.stringify(availableTimeSlots));
+        return availableTimeSlots;
+    }
+  }
+}
+
+const allTimeSlotsAt = (year, month, day) => {
+  const allTimeSlots = [];
+  const startTime = new Date(`${year}-${month}-${day}T09:00:00Z`);
+  for (let i = 0; i < 12; i ++) {
+    const startTimeString = startTime.toISOString();
+    startTime.setMinutes(startTime.getMinutes() + 40);
+    const endTimeString = startTime.toISOString();
+    const timeSlot = {
+      'startTime': startTimeString,
+      'endTime': endTimeString,
+    };
+    allTimeSlots.push(timeSlot);
+    startTime.setMinutes(startTime.getMinutes() + 5);
+  }
+  return allTimeSlots;
+};
 
 export default router;
