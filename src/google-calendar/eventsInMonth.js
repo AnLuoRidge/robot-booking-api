53e6fe64ba1c408a0449d8173a9c5c0a9bd370a3
anluoridge <anluoridge@gmail.com>
2019-11-21 10:59:04 +1100

    Refactor eventsInMonth

--- src/google-calendar/eventsInMonth.js
+++ src/google-calendar/eventsInMonth.js
@@ -1,6 +1,7 @@
 import Calendar from './calendar';
+import getEvents from './getEvents';
 import logger from "../config/winston";
-import errorMsg from "./errorMessages.json";
+
 
 const eventsInMonth = async (year, month) => {
     const daysInMonth = new Date(year, month, 0).getDate();
@@ -9,7 +10,7 @@ const eventsInMonth = async (year, month) => {
     logger.debug(`end: ${year}-${month}-${daysInMonth}T18:00:00.000Z`);
 
     const options = {
-        timeMin: `2019-${month}-01T9:00:00.000Z`,//(new Date()).toISOString(),
+        timeMin: `2019-${month}-01T9:00:00.000Z`,
         timeMax: `2019-${month}-${daysInMonth}T18:00:00.000Z`,
         maxResults: 12 * 31,
         singleEvents: true,
@@ -17,40 +18,9 @@ const eventsInMonth = async (year, month) => {
        calendarId: global.gConfig.GOOGLE_CALENDAR_ID
     };
 
-      const result = await eventList(calendar, options)
-      .then((res) => {
-        const events = res.data.items;
-        logger.debug(`Total events: ${events.length}`);
-        const response = {
-            success: true,
-            events
-        }
-        return response;
-    })
-    .catch(err => {
-        logger.error('The API returned an error: ' + err);
-        const errorMessage = errorMsg.toFill;
-        errorMessage.error = err;
-        return errorMessage;
-    })
-
+      const result = await getEvents(calendar, options);
+      logger.debug(`eventsInMonth.js: ${JSON.stringify(result)} ...`.substr(0, 200));
       return result;
 }
-
-/**
- * A promise wrapper for Google Calendar event list api.
- * @param calendar Authed Google Calendar object.
- * @param options Request body for events.
- * @returns Event list or error messages.
- */
-const eventList = (calendar, options) => new Promise((resolve, reject) => {
-    calendar.events.list(options, (err, res) => {
-    if (err) {
-      reject(err);
-    } else {
-        resolve(res);
-    }
-    })
-  });
 
 export default eventsInMonth;
---
+++ src/google-calendar/getEvents.js
@@ -0,0 +1,26 @@
+import eventList from "./eventList";
+import errMsg from "./errorMessages.json";
+import logger from "../config/winston";
+
+// A helper of eventList()
+const getEvents = async (calendar, options) => {
+    return await eventList(calendar, options)
+      .then((res) => {
+        const events = res.data.items;
+        logger.debug(`Total events: ${events.length}`);
+        const response = {
+            success: true,
+            events
+        };
+        logger.debug(`getEvents.js: ${JSON.stringify(response)} ...`.substr(0, 200));
+        return response;
+    })
+    .catch((err) => {
+        logger.error('The API returned an error: ' + err);
+        const errorMessage = errMsg.toFill;
+        errorMessage.error = err;
+        return errorMessage;
+    })
+}
+
+export default getEvents;
